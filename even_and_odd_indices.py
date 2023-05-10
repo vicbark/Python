e96a914a9097fdc1f7e617b47940613ee8b28455
Victor Barkalov <120062506+vicbark@users.noreply.github.com>
2023-05-10 21:41:30 +0300

    Even and odd indices in a sentenceю Python
    Python. Even and odd indices in a sentence

---
+++ even_and_odd_indices.py
@@ -0,0 +1,6 @@
+#Rabota s prelozeniem, kotoroe vvodit poljzovatelj:
+s2=input("Enter a sentence ")
+even=s2[::2] #võchiljaem simvoli s chetnim indeksom (s 0-ogo indeksa) 
+odd=s2[1::2] #võchiljaem simvoli na nechetnih mestah (s 1-ogo indeksa)
+print(even)
+print(odd)
